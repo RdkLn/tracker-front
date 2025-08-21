c73edc55833ddf5cab56bffc82900bf77efece83
rdk <radlek19@gmail.com>
2025-08-21 15:36:05 +0200

    Added Hero and Header components

---
+++ src/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import DrawerSvg from './assets/drawer.svg?react';
+
+const Header = () => {
+  return (
+    <div className="bg-[#3e3e3e]">
+      <nav className="mx-auto max-w-7xl p-3 flex items-center justify-between lg:px-8">
+        <div className="size-5 ">
+          <DrawerSvg className="stroke-white "></DrawerSvg>
+        </div>
+        <h1 className="w-5/10 text-lg text-center text-white font-bold">Header</h1>
+        <div></div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/features/HeroSection/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="p-5 flex flex-col">
+      <h1 className=" mt-3 font-extrabold text-white text-4xl text-center z-0">
+        Entrenamiento <br /> de hoy
+      </h1>
+      <div className="flex justify-center mt-[-5px]">
+        <button className="bg-indigo-900  w-7/10 max-w-[1024px] hover:bg-indigo-700 text-white p-3 text-sm rounded-full">
+          Ir al entrenamiento de hoy {'->'}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
